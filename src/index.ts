c5bac2110487f6019c5e598ecefe4ee1ec08e19e
okok0415 <dlawjdals11@naver.com>
2021-09-17 16:18:29 +0900

    210917 create blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+import * as CryptoJS from "crypto-js"
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockhash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name: "lim",
-    age: 22,
-    gender: "male"
-};
+const GenesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-}
+let blockchain: [Block] = [GenesisBlock];
 
-console.log(sayHi(person));
+console.log(blockchain)
+
 
 export { };
